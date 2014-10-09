7f3b2492640cb4579fd7f57069b6021c14c83a7c
Dhrubo62 <mohiuddin.fahim49@gmail.com>
2014-10-10 01:46:09 +0600

    my template for C++

---
+++ ddhrubo.cpp
@@ -0,0 +1,51 @@
+//{ ---------- C headers
+# include <cstdio>
+# include <cstring>
+# include <cmath>
+# include <cstdlib>
+# include <cctype>
+//}
+
+//{ ---------- C++ headers
+# include <iostream>
+# include <string>
+# include <algorithm>
+# include <vector>
+# include <queue>
+# include <stack>
+# include <map>
+# include <sstream>
+# include <set>
+//}
+using namespace std;
+
+//{ ---------- Macros
+# define ll long long
+
+# define vint vector <int>
+# define vll vector <long long>
+# define qint queue <int>
+# define qll queue <long long>
+# define pqint priority_queue <int>
+# define pqll priority_queue <long long>
+
+# define pb push_back
+# define sz size()
+
+# define sqr( a ) a*a
+# define cube( a ) a*a*a
+//}
+
+//{ ---------- Movements
+/*int dx[]={1,-1,0,0}, dy[]={0,0,1,-1};*/ // 4 way movement
+/*int dx[]={1,0,-1,0,1,-1,1,-1}, dy[]={0,1,0,-1,1,1,-1,-1};*/ // 8 way movement
+//}
+
+int main() {
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+
+
+
+    return 0;
+}
