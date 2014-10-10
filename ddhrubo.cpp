5a253c4bad94d7a36018ea83fe26cdce99f2bd0b
Dhrubo62 <mohiuddin.fahim49@gmail.com>
2014-10-10 15:32:39 +0600

    Template updated

--- ddhrubo.cpp
+++ ddhrubo.cpp
@@ -19,21 +19,56 @@
 //}
 using namespace std;
 
-//{ ---------- Macros
+//{ ---------- Types
 # define ll long long
-
 # define vint vector <int>
 # define vll vector <long long>
 # define qint queue <int>
 # define qll queue <long long>
 # define pqint priority_queue <int>
 # define pqll priority_queue <long long>
+//}
 
-# define pb push_back
-# define sz size()
+//{ ---------- Stl Functions
+# define pb( a, x ) a.push_back( x )
+# define sz( a ) a.size()
+# define srtp( a ) sort( a.begin(), a.end() )
+# define srtpf( a, c ) sort( a.begin(), a.end(), c )
+//}
 
+//{ ---------- Misc Functions
 # define sqr( a ) a*a
 # define cube( a ) a*a*a
+# define memo( a, x ) memset( a, x, sizeof a )
+# define srt( a, x ) sort( a, a+x )
+# define srtf( a, x, c ) sort( a, a+x, c )
+//}
+
+//{ ---------- Loops
+# define forab( i, a, b ) for( __typeof(a) i=(a); i<=(b); i++ )
+# define rep(i,a) forab( i, 0, a-1 )
+# define arep(i,a) forab( i, 1, a )
+# define forba( i, b, a ) for( __typeof(b) i=(b); i>=(a); i-- )
+# define per(i,a) forba( i, a-1, 0 )
+# define pera(i,a) forba( i, a, 1 )
+//}
+
+//{ ---------- I/O
+# define sint(a) scanf( "%d", &a )
+# define sll(a) scanf( "%lld", &a )
+# define schar(a) scanf( "%c", &a )
+# define sstr(a) scanf( "%s", a )
+# define pcase( a ) printf( "Case %d: ", a )
+# define pcasen( a ) printf( "Case %d:\n", a )
+//}
+
+//{ ---------- I/O Streams
+# define read freopen("input.txt","r",stdin)
+# define write freopen("output.txt","w",stdout)
+//}
+
+//{ ---------- Constants
+# define inf( a, type ) type a = pow( 2, 8*sizeof( type )-1 )-1
 //}
 
 //{ ---------- Movements
@@ -41,9 +76,9 @@ using namespace std;
 /*int dx[]={1,0,-1,0,1,-1,1,-1}, dy[]={0,1,0,-1,1,1,-1,-1};*/ // 8 way movement
 //}
 
+//---------------------------------- Start of the program
+
 int main() {
-//    freopen("input.txt","r",stdin);
-//    freopen("output.txt","w",stdout);
 
 
 
