d5a3c70f315e7ec90cb1ae9f4266348da2b18aa2
Dhrubo62 <mohiuddin.fahim49@gmail.com>
2015-01-25 22:15:40 +0600

    Strongly Connected Components

--- Graph_Theory/SCC Kosaraju.cpp
+++ Graph_Theory/SCC Kosaraju.cpp
@@ -1,60 +1,7 @@
-//{ ---------- C headers
-# include <cstdio>
-# include <cstring>
-# include <cmath>
-# include <cstdlib>
-# include <cctype>
-//}
-
-//{ ---------- C++ headers
-# include <iostream>
-# include <string>
-# include <algorithm>
-# include <vector>
-# include <queue>
-# include <stack>
-# include <map>
-# include <sstream>
-# include <set>
-# include <utility>
-//}
-using namespace std;
-
-//{ ---------- Types
-# define ll long long
-# define ff first
-# define ss second
-//}
-
-//{ ---------- Input
-# define II ( { int a; scanf("%d",&a); a; } )
-# define LL ( { ll a; scanf("%lld",&a); a; } )
-# define DD ( { double a; scanf("%lf",&a); a; } )
-//}
-
-//{ ---------- Misc Functions
-# define sqr( a ) a*a
-# define cube( a ) a*a*a
-# define memo( a, x ) memset( a, x, sizeof a )
-//}
-
-//{ ---------- I/O Streams
-# define read freopen("input.txt","r",stdin)
-# define write freopen("output.txt","w",stdout)
-//}
-
-//{ ---------- Constants
-# define inf( a, type ) type a = pow( 2, 8*sizeof( type )-1 )-1
-//}
-
-//{ ---------- Movements
-/*int dx[]={1,-1,0,0}, dy[]={0,0,1,-1};*/ // 4 way movement
-/*int dx[]={1,0,-1,0,1,-1,1,-1}, dy[]={0,1,0,-1,1,1,-1,-1};*/ // 8 way movement
-//}
-
-//---------------------------------- Start of the program
-
-# define MAX 100
+/*
+Name: Strongly Connected Components (Kosaraju's algorithm)
+Catagory: Graph Theory
+*/
 
 vector <int> g[MAX], gr[MAX], component;
 bool visit[MAX];
