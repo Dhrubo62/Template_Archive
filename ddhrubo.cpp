7bcb2694d977abba2137c48d545f027738b700a2
Dhrubo62 <mohiuddin.fahim49@gmail.com>
2015-02-20 17:06:05 +0600

    tmp

--- ddhrubo.cpp
+++ ddhrubo.cpp
@@ -21,48 +21,48 @@
 using namespace std;
 
 //{ ---------- Types
-# define ll long long
-# define ff first
-# define ss second
-# define vi vector < int >
-# define vll vector < ll >
-# define pii pair < int , int >
-# define pll pair < ll, ll >
+# define ll                 long long
+# define ff                 first
+# define ss                 second
+# define vi                 vector < int >
+# define vll                vector < ll >
+# define pii                pair < int , int >
+# define pll                pair < ll, ll >
 //}
 
 //{ ---------- Input
-# define II ( { int a; scanf("%d",&a); a; } )
-# define LL ( { ll a; scanf("%lld",&a); a; } )
-# define DD ( { double a; scanf("%lf",&a); a; } )
+# define II                 ( { int a; scanf("%d",&a); a; } )
+# define LL                 ( { ll a; scanf("%lld",&a); a; } )
+# define DD                 ( { double a; scanf("%lf",&a); a; } )
 //}
 
 //{ ---------- Macro
-# define sqr( a ) a*a
-# define cube( a ) a*a*a
-# define memo( a, x ) memset( a, x, sizeof a )
-# define all(a) a.begin () , a. end ()
-# define clr(a) a.clear()
-# define sz(a) a.size()
-# define pb push_back
+# define sqr( a )           a*a
+# define cube( a )          a*a*a
+# define memo( a, x )       memset( a, x, sizeof a )
+# define all(a)             a.begin () , a. end ()
+# define clr(a)             a.clear()
+# define sz(a)              a.size()
+# define pb                 push_back
 //}
 
 //{ ---------- Loops
-# define forab(i,a,b) for( __typeof (a) i = a ; i <= b ; i++ )
-# define forba(i,a,b) for( __typeof (a) i = a ; i >= b ; i-- )
-# define rep(i,n) forab(i,0,n-1)
-# define repn(i,n) forab(i,1,n)
-# define repr(i,n) forba(i,n-1,0)
-# define repnr(i,n) forba(i,n,1)
-# define forstl(i, s) for ( __typeof ((s).end ()) i = (s).begin (); i != (s).end (); i++ )
+# define forab(i,a,b)       for( __typeof (a) i = a ; i <= b ; i++ )
+# define forba(i,a,b)       for( __typeof (a) i = a ; i >= b ; i-- )
+# define rep(i,n)           forab(i,0,n-1)
+# define repn(i,n)          forab(i,1,n)
+# define repr(i,n)          forba(i,n-1,0)
+# define repnr(i,n)         forba(i,n,1)
+# define forstl(i, s)       for ( __typeof ((s).end ()) i = (s).begin (); i != (s).end (); i++ )
 //}
 
 //{ ---------- I/O Streams
-# define read freopen("input.txt","r",stdin)
-# define write freopen("output.txt","w",stdout)
+# define read               freopen("input.txt","r",stdin)
+# define write              freopen("output.txt","w",stdout)
 //}
 
 //{ ---------- Constants
-# define inf( a, type ) type a = pow( 2, 8*sizeof( type )-1 )-1
+# define inf( a, type )     type a = pow( 2, 8*sizeof( type )-1 )-1
 //}
 
 //{ ---------- Movements
@@ -73,6 +73,7 @@ using namespace std;
 //---------------------------------- Start of the program
 
 int main() {
+
 
 
     return 0;
